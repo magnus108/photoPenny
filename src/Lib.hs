aea719175b8ee25d660e54551667f5447521a76d
Magnus Møller <magnus108@me.com>
2019-05-02 08:35:14 +0000

    eh okay prepare big refac

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,55 +8,62 @@ import Graphics.UI.Threepenny.Core
 
 import System.Directory
 
-import qualified PhotoShake as PS
-
-import Development.Shake
+import PhotoShake
+import Photographee
+import PhotoShake.ShakeConfig
 
 import Control.Monad
+
+
+import Development.Shake
 import Control.Exception
-
-import System.Exit
-import System.Process
-
-import Data.HashMap.Lazy
 import Development.Shake.FilePath
 
 import System.FSNotify hiding (defaultConfig)
-import Control.Concurrent (newChan, readChan, dupChan, forkIO, getChanContents)
-import Control.Monad (forever)
+import Control.Concurrent 
 
-import Debug.Trace
+
 
 someFunc :: Int -> IO ()
 someFunc port = do
-    config <- PS.readConfigFile "config.cfg"
-    dirs <- listDirectory $ PS._dumpDir config
+    config <- toShakeConfig "config.cfg"
+    dirs <- listDirectory $ _dumpDir config
     withManager $ \mgr -> do
         -- start a watching job (in the background)
+        msgChan <- newChan
         outChan <- newChan
         dumpChan <- newChan
-        watchDirChan
+        locationChan <- newChan
+        _ <- watchDirChan
             mgr
             "/home/magnus/Documents/projects/photoPenny/out"
             (const True)
             outChan
 
-        watchDirChan
-            mgr          -- manager
+        _ <- watchDirChan
+            mgr
             "/home/magnus/Documents/projects/photoPenny/dump"
-            (const True) -- predicate
+            (\x -> case x of
+                Added _ _ _ -> True
+                _ -> False)
             dumpChan
+        
+        _ <- watchDirChan
+            mgr
+            "/home/magnus/Documents/projects/photoPenny/locations"
+            (const True)
+            locationChan
 
         startGUI
             defaultConfig { jsCustomHTML = Just "index.html"
                       , jsStatic = Just "static"
                       , jsPort = Just port
                       } 
-            (setup config dirs dumpChan)
+            (setup config dirs dumpChan locationChan msgChan)
 
 mkButton :: String -> String -> UI (Element, Element)
-mkButton title id = do
-    button <- UI.button #. "button" # set UI.id_ id #+ [string title]
+mkButton titl ident = do
+    button <- UI.button #. "button" # set UI.id_ ident #+ [string titl]
     view <- UI.div #. "control" #+ [element button]
     return (button, view)
 
@@ -71,47 +78,85 @@ mkLabel s =
     UI.p #. "has-text-info has-text-weight-bold is-size-5" # set UI.text s
 
 
-setup :: PS.Config -> [FilePath] -> EventChannel -> Window -> UI ()
-setup config dumps dumpChan w = do
-    (button, view) <- mkButton "Run build" "thisId"
+mkInput :: Chan String -> String -> UI (Element, Element)
+mkInput chan s = do
+    input <- UI.input #. "input" # set UI.type_ "text" 
+    view <- UI.div #. "field is-horizontal" #+
+        [ UI.div #. "field-label is-normal" #+
+            [ UI.label #. "label" # set UI.text s ]
+        , UI.div #. "field-body" #+
+            [ UI.div #. "field" #+
+                [ UI.p #. "control" #+ [ element input ] ]
+            ]
+        ]
+
+    on UI.keyup input $ \_ -> do
+        val <- get value input
+        liftIO $ writeChan chan val
+        
+    return (input, view)
+
+
+setup :: ShakeConfig -> [FilePath] -> EventChannel -> EventChannel -> Chan String -> Window -> UI ()
+setup config dumps dumpChan locationChan msgChan w = do
+    (_, view) <- mkButton "Run build" "thisId"
     (button1, view1) <- mkButton "Select folder" "thisId1"
  
     dumpChanges <- UI.p
 
     dump <- mkSection
-        [ mkLabel "dumpDir"
-        , UI.p # set UI.text (PS._dumpDir config)
+        [ mkLabel "DumpDir"
+        , UI.p # set UI.text (_dumpDir config)
         , UI.p # set UI.text (head dumps)
         , element dumpChanges
         ]
 
     out <- mkSection
-        [ mkLabel "outDir"
-        , UI.p # set UI.text (PS._outDir config)
+        [ mkLabel "OutDir"
+        , UI.p # set UI.text (_outDir config)
         ]
+
+    locationChanges <- UI.select
 
     location <- mkSection
-        [ mkLabel "location"
-        , UI.p # set UI.text (PS._location config)
+        [ mkLabel "Location"
+        , UI.p # set UI.text (_location config)
+        , UI.div #. "field" #+ 
+            [ UI.div #. "control" #+
+                [ UI.div #. "select" #+ 
+                    [ element locationChanges
+                    ]
+                ]
+            ]
         ]
+
+    (_, inputView) <- mkInput msgChan "id" 
+    
+    ident <- UI.p
 
     photoConfig <- mkSection 
-        [ mkLabel "photographeeId"
-        , UI.p # set UI.text "not sat"
+        [ mkLabel "PhotographeeId"
+        , element ident
+        , element inputView
         ]
 
-    section <- mkSection [element view1, element view
+    msg <- UI.p
+    section <- mkSection [
+             element view1
+            , element view
+            , element msg
             , element dump
             , element out
             , element location
             , element photoConfig
             ]
 
-    getBody w #+ [element section] 
+    _ <- getBody w #+ [element section] 
          
-   -- onElementId "thisId" "click"
-    --    (element msg # set text "Clicked")
-     --   msg
+    _ <- onElementId "thisId" "click" config (Photographee "lol" "lol" "lol")
+        (element msg # set text "Clicked")
+        msg
+     --
      --
     --selectFolder "thisId1" "click" $ \x -> do
     --    putStrLn "lol"
@@ -126,9 +171,16 @@ setup config dumps dumpChan w = do
 
         runFunction $ ffi "require('electron').remote.dialog.showOpenDialog({properties: ['openDirectory']}, %1)" callback
 
-    dumpChan' <- liftIO $ dupChan dumpChan
 
+    dumpChan' <- liftIO $ dupChan dumpChan
     void $ liftIO $ forkIO $ receiveDumps w dumpChan' dumpChanges
+
+    locationChan' <- liftIO $ dupChan locationChan
+    void $ liftIO $ forkIO $ receiveLocations w locationChan' locationChanges
+
+    msgChan' <- liftIO $ dupChan msgChan
+    void $ liftIO $ forkIO $ receiveMsg w msgChan' ident
+    
     --
     --on UI.click button $ \_ -> do 
       --  runFunction $ ffi "require('electron').shell.openItem('/home/magnus/Downloads/fetmule.jpg')"
@@ -139,24 +191,40 @@ receiveDumps w events dumpFiles = do
     messages <- getChanContents events
     forM_ messages $ \msg -> do
         runUI w $ do
-          element dumpFiles #+ [UI.p # set UI.text (show msg)]
+          _ <- element dumpFiles #+ [UI.p # set UI.text (takeFileName $ eventPath msg)]
+          flushCallBuffer
+
+receiveLocations :: Window -> EventChannel -> Element -> IO ()
+receiveLocations w events locationFiles = do
+    messages <- getChanContents events
+    forM_ messages $ \msg -> do
+        runUI w $ do
+          _ <- element locationFiles #+ [UI.option # set UI.text (takeFileName $ eventPath msg)]
+          flushCallBuffer
+
+receiveMsg :: Window -> Chan String -> Element -> IO ()
+receiveMsg w events ident = do
+    messages <- getChanContents events
+    forM_ messages $ \msg -> do
+        runUI w $ do
+          _ <- element ident # set UI.text msg
           flushCallBuffer
 
 
-selectFolder :: String -> String -> (FilePath -> IO ()) -> UI ()
-selectFolder elid event complete = do
-    callback <- ffiExport complete
-    runFunction $ ffi "$(%1).on(%2,require('electron').remote.dialog.showOpenDialog({properties: ['openDirectory']}, %3))" ("#"++elid) event callback
+--selectFolder :: String -> String -> (FilePath -> IO ()) -> UI ()
+--selectFolder elid event complete = do
+--    callback <- ffiExport complete
+--    runFunction $ ffi "$(%1).on(%2,require('electron').remote.dialog.showOpenDialog({properties: ['openDirectory']}, %3))" ("#"++elid) event callback
 
 
-onElementId :: String -> String -> UI void -> Element -> UI ()
-onElementId elid event handler err = do
+onElementId :: String -> String -> ShakeConfig -> Photographee -> UI void -> Element -> UI ()
+onElementId elid event config photographee handler err = do
     window   <- askWindow
     exported <- ffiExport $ do
-        lol <- try PS.someFunc :: IO (Either ShakeException ())
-
-        case lol of
-            Left x -> runUI window (element err # set text (displayException (shakeExceptionInner x))) >> return ()
+        shakeIO <- try $ myShake config photographee :: IO (Either ShakeException ())
+        case shakeIO of
+            Left x -> runUI window 
+                (element err # set text (displayException (shakeExceptionInner x))) >> return ()
             Right _ -> runUI window handler >> return ()
    
     runFunction $ ffi "$(%1).on(%2,%3)" ("#"++elid) event exported
