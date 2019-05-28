23be03bef260a988844fba2c10127d551ee3f149
Magnus Møller <magnus108@me.com>
2019-05-28 21:02:51 +0000

    DO something about foldable

--- src/Shooting.hs
+++ src/Shooting.hs
@@ -5,7 +5,7 @@ module Shooting
     ) where
 
 import Prelude hiding (writeFile)
-import Data.ByteString.Lazy hiding (take)
+import Data.ByteString.Lazy hiding (take, putStrLn)
 
 import PhotoShake.Shooting
 
@@ -14,7 +14,6 @@ import Graphics.UI.Threepenny.Core
 
 import Elements
 
-import Control.Arrow
 import Utils.ListZipper 
 import Utils.Comonad
 
@@ -22,41 +21,50 @@ import PhotoShake.ShakeConfig
 
 
 shootingSection :: FilePath -> FilePath -> UI Element
-shootingSection config shootingConfig = mkSection
+shootingSection root config = mkSection
                                 [ mkLabel "Shooting Type"
-                                , mkRadioShootings config shootingConfig
+                                , mkRadioShootings root config
                                 ]
 
 
 mkRadioShootings :: FilePath -> FilePath -> UI Element
-mkRadioShootings _ conf = do 
-    shootings <- liftIO $ getShootings conf
-    view <- mkRadioGroup "foobar" conf (unShootings shootings)
-    return view
-
-
-mkRadioGroup :: String -> FilePath -> ListZipper Shooting -> UI Element
-mkRadioGroup name config xs = do
-    let labels x = UI.string (show (focus x))
-    let inputs x = do
-            y <- UI.input # set UI.type_ "radio" # set UI.name name
-            on UI.checkedChange y $ \_ -> do
+mkRadioShootings _ config = do 
+    shootings <- liftIO $ getShootings config
+    let group' = RadioGroup 
+            { action = \x _ -> do
                     liftIO $ writeFile config $ encode (Shootings x)
                     return ()
-            return y
-
-    let displays = extend (labels &&& inputs) xs
-    let selected x = x # set (UI.attr "checked") "true" 
-    let withSelected = mapFocus (second selected) displays
-
-    let radio (x,y) = UI.label #. "radio" #+ [y, x]
-
-    let widgets = fmap radio withSelected
-    let l = lefts widgets
-    let x = focus widgets
-    let r = rights widgets
-
-    view <- UI.div #. "control" #+ (l ++ [x] ++ r)
-
-
+            , view' = \x -> UI.string (show (focus x))
+            , title' = "foobar"
+            , items = unShootings shootings
+            }
+    view <- mkRadioGroup group'
     return view
+
+
+mkRadioGroup :: Eq a => RadioGroup a -> UI Element
+mkRadioGroup x = do
+    let widgets = extend 
+            (\zipper -> do
+                    input' <- UI.input # set UI.type_ "radio" # set UI.name (title' x) 
+                    input <- if (zipper == (items x)) then
+                            set (UI.attr "checked") "" (element input')
+                        else
+                            return input'
+                        
+                    on UI.checkedChange input ((action x) zipper)
+                    label <- (view' x) zipper  
+                    view <- UI.label #. "radio" #+ [element input, element label]
+                    return view
+            ) (items x)
+
+    view <- UI.div #. "control" #+ (toList widgets)
+    return view
+
+
+data RadioGroup a = RadioGroup
+    { action :: ListZipper a -> Bool -> UI ()
+    , view' :: ListZipper a -> UI Element
+    , title' :: String
+    , items :: ListZipper a
+    }
