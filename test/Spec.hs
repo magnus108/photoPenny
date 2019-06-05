34f541383b526819eb25f83bf109cc54dafa8239
Magnus Møller <magnus108@me.com>
2019-06-05 00:04:59 +0000

    testit?

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Test.WebDriver
+
+import Control.Concurrent.Async
+import Lib
+
+chromeConfig :: WDConfig
+chromeConfig = useBrowser chrome defaultConfig
+
+runSessionThenClose :: WD a -> IO a
+runSessionThenClose action = runSession chromeConfig . finallyClose $ action
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    race_ (setup 9000 "")
+        (runSessionThenClose $ do                      
+            _ <- setImplicitWait 1230023
+            openPage "http://localhost:9000"
+            _ <- setImplicitWait 1234
+            searchInput <- findElem ( ByCSS "input[type='text']" )  
+            sendKeys "1234x!" searchInput)
+
