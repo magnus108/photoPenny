fea9f9ba842e748fb20b4369b41eb6cf6f350b56
Magnus Møller <magnus108@me.com>
2019-06-06 10:58:44 +0000

    hhmmmmmm

--- src/Photographer.hs
+++ src/Photographer.hs
@@ -32,7 +32,14 @@ photographerSection config = do
                                     ]
                     return (False, gg)
 
-            Photographers y -> do
+            UnApprovedPhotographers y -> do
+                        gg <- mkSection [ mkLabel "Fotograf"
+                                        , mkApprovedPhotographers config y $ \z _ ->
+                                                liftIO $ setPhotographers config $ ApprovedPhotographers z
+                                        ]
+                        return (False, gg)
+
+            ApprovedPhotographers y -> do
                         gg <- mkSection [ mkLabel "Fotograf"
                                         , mkSelectPhotographers config y
                                         ]
@@ -46,12 +53,25 @@ mkPhotographersImporter config = do
         liftIO $ importPhotographers config file
     return view
 
+mkApprovedPhotographers :: ShakeConfig -> ListZipper Photographer -> (ListZipper Photographer-> () -> UI ()) -> UI Element
+mkApprovedPhotographers config y cb = do
+    (button, buttonView) <- mkButton "approvePhotographers" "ok"
+
+    on UI.click button (cb y)
+
+    view <- UI.div #+
+                [ mkSelectPhotographers config y
+                , element buttonView
+                ]
+    return view
+
+
 
 mkSelectPhotographers :: ShakeConfig -> ListZipper Photographer -> UI Element
 mkSelectPhotographers config y = do 
     let group' = RadioGroup 
             { action = \x _ -> do
-                    liftIO $ setPhotographers config $ Photographers x
+                    liftIO $ setPhotographers config $ ApprovedPhotographers x
             , view' = \x -> UI.string (name (focus x))
             , title' = "photographers"
             , items = y 
